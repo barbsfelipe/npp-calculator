c09345db423b09d5b8f31b7f87015ab573259b73
barbsfelipe <felipebarbosamd@gmail.com>
2026-08-17 21:47:22 -0400

    Inicializa projeto Capacitor do app mobile

---
+++ app-mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.npp.calculadora',
+  appName: 'Calculadora de NPP',
+  webDir: 'www'
+};
+
+export default config;
